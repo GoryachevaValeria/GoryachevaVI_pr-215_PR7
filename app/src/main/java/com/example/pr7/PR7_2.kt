e5de21a4acf9f2badf3f5064ffa9ed7227b86f93
GoryachevaValelia <goryachevav01@mail.ru>
2021-04-19 22:10:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pr7/PR7_2.kt
@@ -0,0 +1,11 @@
+package com.example.pr7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PR7_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_p_r7_2)
+    }
+}
